8a3589dc49553acf0e2fff4cf437e69ef694537c
qaz9391 <135338851+qaz9391@users.noreply.github.com>
2025-04-09 15:40:47 +0800

    Update 8.counter.php

--- 8.counter.php
+++ 8.counter.php
@@ -1,10 +1,12 @@
 <?php
-    session_start();
-    if (!isset($_SESSION["counter"]))
-        $_SESSION["counter"]=1;
-    else
-        $_SESSION["counter"]++;
-
-    echo "counter=".$_SESSION["counter"];
-    echo "<br><a href=9.reset_counter.php>重置counter</a>";
+    session_start();                                           //啟動session後才能使用儲存裡面的資料
+    if (!isset($_SESSION["counter"]))                          //這段是檢查裡面有沒有已經有counter（這個是計數器）
+    {
+        $_SESSION["counter"] = 2;                              //如果沒有的話，代表第一次進入我把它設定為counter為2
+    } else 
+    {
+        $_SESSION["counter"]++;                                //如果有就是進來過，就讓counter+2
+    }
+    echo "counter = " . $_SESSION["counter"];                  //顯示目前它的值
+    echo "<br><a href='9.reset_counter.php'>重置 counter</a>";  //顯示一個超連結，然後連到畫面中的頁面（用來重置counter）
 ?>
