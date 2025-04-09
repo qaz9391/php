f6905b9a5832d64c4557393d9af1cccd1f6ede45
qaz9391 <135338851+qaz9391@users.noreply.github.com>
2025-04-09 15:25:00 +0800

    Update 5✅.while.php

--- 5.while.php
+++ 5.while.php
@@ -1,10 +1,7 @@
 <?php
-   #mysqli_connect() 建立資料庫連結
-   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");
-   #mysqli_query() 從資料庫查詢資料
-   $result=mysqli_query($conn, "select * from user");
-   #mysqli_fetch_array() 從查詢出來的資料一筆一筆抓出來
-   while ($row=mysqli_fetch_array($result)) {
+   $conn=mysqli_connect("db4free.net", "immust", "immustimmust", "immust");    #mysqli_connect()這段是建立資料庫連結
+   $result=mysqli_query($conn, "select * from user");                          #mysqli_query()這函數是在資料庫裡面查詢資料
+   while ($row=mysqli_fetch_array($result)) {                                  #mysqli_fetch_array()從查詢出來的資料一筆一筆抓出來
      echo $row["id"]." ".$row["pwd"]."<br>";
    } 
 ?>
