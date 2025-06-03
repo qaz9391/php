33befe6919e39db2c4b37b8a1cca2f831e0d942d
qaz9391 <135338851+qaz9391@users.noreply.github.com>
2025-06-04 02:59:59 +0800

    Create 1 ✅.html.php

---
+++ 1 ✅.html.php
@@ -0,0 +1,10 @@
+<html>
+    <head><title>html+php</title></head>      
+    <body>
+        <?php echo "Hello world!"; ?> <!-- 這裡是PHP語法，會顯示「Hello world!」在畫面上 -->
+        <h1>html</h1> <!-- 這是網頁的標題，會顯示大字「html」 -->
+        <?php echo "Hello world!"; ?> <!-- 又印一次「Hello world!」 -->
+        <h1>html</h1> <!-- 再顯示一次網頁標題 -->
+        <?php echo "Hello world!"; ?> <!-- 第三次印出「Hello world!」，總共出現三次 -->
+    </body>
+</html>
